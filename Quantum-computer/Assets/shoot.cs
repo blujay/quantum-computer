923b7cb8c00b0a005c2cb8d6a8c9786a7db304e1
Maf'j Alvarez <jay@rootinteractive.com>
2019-09-27 11:23:44 +0100

    adding project files Unity

---
+++ Quantum-computer/Assets/shoot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+
+public Transform RHand;
+
+    // Start is called before the first frame update
+    public void shootObject(){
+        var direction = RHand.transform.rotation * Vector3.up * 100f;
+        this.gameObject.GetComponent<Rigidbody>().AddForce(direction);
+        Debug.Log(direction);
+    }
+}
