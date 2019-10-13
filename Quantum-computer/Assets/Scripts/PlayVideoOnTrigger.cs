de37557bb7959f85769306cf3ddc3e9ce77014d6
Andy Baker <andy@andybak.net>
2019-10-13 15:12:46 +0100

    Fix entangler warning. Add video trigger to quanum station.

---
+++ Quantum-computer/Assets/Scripts/PlayVideoOnTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Video;
+
+public class PlayVideoOnTrigger : MonoBehaviour
+{
+
+    public UnityEvent TriggerEnterEvent;
+    public UnityEvent TriggerExitEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TriggerEnterEvent.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        TriggerExitEvent.Invoke();
+    }
+
+}
