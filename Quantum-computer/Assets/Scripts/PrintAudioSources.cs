26fd879bfe07eadb9775c94a86ced65a461504a0
EllieWatts1415 <eleanorwatts1415@hotmail.com>
2019-10-17 17:25:20 +0100

    routed all audio through mixer

---
+++ Quantum-computer/Assets/Scripts/PrintAudioSources.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+ 
+ public class PrintAudioSources : MonoBehaviour
+ {
+ 
+ 	AudioSource[] sources;
+             
+             void Start () {
+             	Debug.Log("Logging all audio sources in the scene:");
+                //Get every single audio sources in the scene.
+		
+                sources = GameObject.FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+		Debug.Log("Number of audio sources:");
+		Debug.Log(sources.Length);
+		foreach(AudioSource audioSource in sources)
+		{
+			Debug.Log("source: " + audioSource.name + "  parent: " + audioSource.transform.parent.name + "  root: " + audioSource.transform.root.name);
+			if (audioSource.outputAudioMixerGroup != null) {
+				Debug.Log("output: " + audioSource.outputAudioMixerGroup.name);
+			} else {
+				Debug.Log("No set output");
+			}
+		}
+                 
+             }
+         
+   
+ }
