cd4a38221ed4edeea74990b49258f22cc7f52222
Andy Baker <andy@andybak.net>
2019-11-08 14:34:44 +0000

    GoTween

---
+++ Quantum-computer/Assets/GoTween/GokitTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GokitTest : MonoBehaviour
+{
+
+    void Start()
+    {
+        transform.positionTo(5f, new Vector3(3, 3, 3));
+    }
+
+    void Update()
+    {
+        
+    }
+}
